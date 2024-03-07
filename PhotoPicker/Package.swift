8f74a468c42f81254328c8a3537a6914f4d56006
hanhyeongyu <kss1022hhh@naver.com>
2024-03-07 17:58:45 +0900

    Gallery -> PhotoPicker

--- PhotoPicker/Package.swift
+++ PhotoPicker/Package.swift
@@ -10,23 +10,128 @@ let package = Package(
         .library(
             name: "PhotoPicker",
             targets: ["PhotoPicker"]),
+        .library(
+            name: "Gallery",
+            targets: ["Gallery"])
+        ,
+        .library(
+            name: "GalleryUtils",
+            targets: ["GalleryUtils"]
+        ),
+        .library(
+            name: "Albums",
+            targets: ["Albums"]
+        ),
+        .library(
+            name: "Permission",
+            targets: ["Permission"]
+        ),
+        .library(
+            name: "Selection",
+            targets: ["Selection"]
+        ),
+        .library(
+            name: "AlbumRepository",
+            targets: ["AlbumRepository"]
+        ),
+        .library(
+            name: "AlbumEntity",
+            targets: ["AlbumEntity"]
+        ),
+        .library(
+            name: "PermissionTestSupport",
+            targets: ["PermissionTestSupport"]
+        ),
+        .library(
+            name: "AlbumRepositoryTestSupport",
+            targets: ["AlbumRepositoryTestSupport"]
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/DevYeom/ModernRIBs", branch: "main"),
-        .package(path: "../Gallery"),
+        .package(path: "../Platform"),
     ],
     targets: [
         .target(
             name: "PhotoPicker",
             dependencies: [
                 "ModernRIBs",
-                .product(name: "Gallery", package: "Gallery"),
-                .product(name: "Permission", package: "Gallery"),
-                .product(name: "AlbumRepository", package: "Gallery")
+                "Gallery",
+                "Permission",
+                "AlbumRepository",
+            ]
+        ),
+        .target(
+            name: "Gallery",
+            dependencies: [
+                .product(name: "UIUtils", package: "Platform"),
+                "ModernRIBs",
+                "GalleryUtils",
+                "Permission",
+                "Selection",
+                "AlbumRepository",
+                "Albums"
+            ]
+        ),
+        .target(
+            name: "GalleryUtils",
+            dependencies: [
+                .product(name: "UIUtils", package: "Platform"),
+            ]
+        ),
+        .target(
+            name: "Albums",
+            dependencies: [
+                .product(name: "UIUtils", package: "Platform"),
+                "ModernRIBs",
+                "GalleryUtils",
+                "AlbumRepository",
+            ]
+        ),
+        .target(
+            name: "Permission"
+        ),
+        .target(
+            name: "Selection",
+            dependencies: [
+                "AlbumEntity"
+            ]
+        ),
+        .target(
+            name: "AlbumRepository",
+            dependencies: [
+                .product(name: "CombineUtils", package: "Platform"),
+                "AlbumEntity"
+            ]
+        ),
+        .target(
+            name: "AlbumEntity"
+        ),
+        .target(
+            name: "PermissionTestSupport",
+            dependencies: [
+                "Permission"
+            ]
+        ),
+        .target(
+            name: "AlbumRepositoryTestSupport",
+            dependencies: [
+                "AlbumRepository"
+            ]
+        ),
+        .testTarget(
+            name: "GalleryTests",
+            dependencies: [
+                "Gallery",
+                "Albums",
+                "PermissionTestSupport",
+                "AlbumRepositoryTestSupport",
+                .product(name: "RIBsTestSupports", package: "Platform")
             ]
         ),
         .testTarget(
             name: "PickerTests",
-            dependencies: ["PhotoPicker"]),
+            dependencies: ["PhotoPicker"]
+        ),
     ]
 )
